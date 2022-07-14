73613533959956b3f1ed73acb1e9c665daf042ac
Julie Heckler <july.heckler@gmail.com>
2022-07-14 12:26:25 +0200

    form creation

--- src/pages/EmployeeCreation/index.jsx
+++ src/pages/EmployeeCreation/index.jsx
@@ -3,7 +3,19 @@ import DatePicker from 'react-date-picker/dist/entry.nostyle';
 import React, { useState } from 'react';
 
 function EmployeeCreation(){
-    const [value, onChange] = useState(new Date())
+    const [birthValue, setBirthValue] = useState(new Date())
+    const [startValue, setStartValue] = useState(new Date())
+    const [isValidZip, setIsValidZip] = useState(true)
+
+    function checkZipCode(e){
+        const zipCode = /(^[0-9]{5}$)/;
+        const testZip = zipCode.test(e.target.value)
+        console.log(isValidZip) 
+
+        if (testZip===false) return setIsValidZip(false)
+
+        setIsValidZip(true)
+    }
 
     return(
         <div className='employee_creation'>
@@ -12,22 +24,22 @@ function EmployeeCreation(){
             <form className='create_form'>
                 <label>
                     Firstname
-                    <input type='text' id='firstname'/>
+                    <input type='text' id='firstname' />
                 </label>
                 <label>
                     Lastname
-                    <input type='text' id='lastname'/>
+                    <input type='text' id='lastname' />
                 </label>
                 <label>
                     Date of Birth
-                    <DatePicker onChange={onChange} value={value} locale='en-EN'/>
+                    <DatePicker onChange={setBirthValue} value={birthValue} locale='en-EN'/>
                 </label>
                 <label>
                     Start Date
-                    <DatePicker onChange={onChange} value={value} locale='en-EN'/>
+                    <DatePicker onChange={setStartValue} value={startValue} locale='en-EN'/>
                 </label>
-                <div id='adress'>
-                    <p>Adress</p>
+                <div id='address'>
+                    <p>Address</p>
                     <label>
                         Street
                         <input type='text' id='street'/>
@@ -40,10 +52,11 @@ function EmployeeCreation(){
                         State
                         <input type='text' id='state'/>
                     </label>
-                    <label>
+                    <label id='zip'>
                         Zip Code
-                        <input type='number' id='zip_code'/>
+                        <input type='number' id='zip_code' onChange={checkZipCode}/>
                     </label>
+                    {!isValidZip? <div className='invalid_field'>Invalid Zip Code</div> : null}
                 </div>
                     <label>
                         Department
