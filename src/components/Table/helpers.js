afc0656b1e040b24192fd4891d41dd7c4c016965
Julie Heckler <july.heckler@gmail.com>
2022-08-12 19:30:59 +0200

    sort functions file creation

---
+++ src/components/Table/helpers.js
@@ -0,0 +1,20 @@
+export function sortStartDates(a,b){
+    if(new Date(b.startDate) > new Date(a.startDate)){
+        return 1
+    }
+    if(new Date(b.startDate) < new Date(a.startDate)){
+        return -1
+    }
+    return 0
+}
+
+export function sortBirthDates(a,b){
+    if(new Date(b.dateBirth) > new Date(a.dateBirth)){
+        return 1
+    }
+    if(new Date(b.dateBirth) < new Date(a.dateBirth)){
+        return -1
+    }else{
+        return 0
+    }
+}
--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,31 +1,11 @@
 import MaterialTable from 'material-table';
 import { ThemeProvider, createTheme } from '@mui/material';
 import SortIcon from '../SortIcon';
+import { sortStartDates, sortBirthDates } from './helpers'
 
 function Table(){
     const theme = createTheme();
  
-    function sortStartDates(a,b){
-        if(new Date(b.startDate) > new Date(a.startDate)){
-            return 1
-        }
-        if(new Date(b.startDate) < new Date(a.startDate)){
-            return -1
-        }
-        return 0
-    }
-
-    function sortBirthDates(a,b){
-        if(new Date(b.dateBirth) > new Date(a.dateBirth)){
-            return 1
-        }
-        if(new Date(b.dateBirth) < new Date(a.dateBirth)){
-            return -1
-        }else{
-            return 0
-        }
-    }
-
     const columns = [
         { title: 'First Name', field: 'firstname' },
         { title: 'Last Name', field: 'lastname' },
